a941ff4c322f0bb025bc3ce5db03b0b0270a6325
Lewis Barnes <362490@edu.cwa.ac.uk>
2018-10-15 21:49:51 +0100

    Added doc class, predicting party

---
+++ BayesClassifier/Doc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BayesClassifier
+{
+    public class Doc
+    {
+        public string FileName;
+        private static List<string> stopWords;
+
+        public List<string> Words;
+        public Dictionary<string, int> WordFrequencies;
+        public Dictionary<string, float> WordProbabilities;
+
+        public Doc(string fileName)
+
+        {
+            AddStopWords();
+            FileName = fileName;
+
+            Words = new List<string>();
+            WordFrequencies = new Dictionary<string, int>();
+            WordProbabilities = new Dictionary<string, float>();
+
+            var wordString = new string(File.ReadAllText(FileName).ToCharArray());
+            var sc = new StringScanner();
+            wordString = sc.RemovePunctuation(ref wordString);
+            wordString = wordString.Replace("\0", string.Empty);
+            foreach (string word in wordString.Split(' '))
+            {
+                if (word != string.Empty)
+                {
+                    if (!stopWords.Any(s => s == word)) Words.Add(word);
+                }
+            }
+            GetWordFrequencies(Words);
+            foreach (var kvp in WordFrequencies)
+            {
+                WordProbabilities[kvp.Key] = (float)kvp.Value / (float)Words.Count();
+            }
+            WordFrequencies = WordFrequencies.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+
+        private void GetWordFrequencies(List<string> words)
+        {
+            foreach (string word in Words)
+            {
+                if (WordFrequencies.TryGetValue(word, out int freq))
+                {
+                    WordFrequencies[word] = freq + 1;
+                }
+                else
+                {
+                    WordFrequencies[word] = 1;
+                }
+            }
+        }
+
+        private static void AddStopWords()
+        {
+            if (stopWords == null)
+            {
+                stopWords = new List<string>();
+                var words = File.ReadAllText("stopwords.txt").Replace("\r\n", " ");
+                foreach (string w in words.Split(' '))
+                {
+                    stopWords.Add(w);
+                }
+            }
+        }
+    }
+}
--- BayesClassifier/Program.cs
+++ BayesClassifier/Program.cs
@@ -19,9 +19,12 @@ namespace BayesClassifier
                 new TrainingDoc("Conservative27thMay2015.txt", Category.CONSERVATIVE),
                 new TrainingDoc("Labour6thNov2007.txt", Category.LABOUR),
                 new TrainingDoc("Labour26thNov2003.txt", Category.LABOUR)};
-
+            Doc unknownDoc = new Doc("unknownLabour.txt");
             Dictionary<Category, Dictionary<string, int>> partyWordFrequencies = new Dictionary<Category,Dictionary<string, int>>();
             Dictionary<Category, List<Word>> partyWords = new Dictionary<Category,List<Word>>();
+
+            Dictionary<Category, double> overallProbs = new Dictionary<Category, double>();
+
             foreach (Category c in Enum.GetValues(typeof(Category)))
             {
                 partyWordFrequencies[c] = DictionaryHelper.MergeDictionaries(trainingSet.Where(s => s.Category == c).Select(w => w.WordFrequencies).ToArray());
@@ -37,6 +40,32 @@ namespace BayesClassifier
                     partyWords[c].Add(new Word(pair.Key, pair.Value, (double)(pair.Value + 1) / ((double)partyWordFrequencies[c].Values.Sum() + (double)partyWordFrequencies[c].Keys.Count)));
                 }
             }
+            Dictionary<Category, double> priorProbs = new Dictionary<Category, double>();
+            priorProbs[Category.CONSERVATIVE] = 0.4;
+            priorProbs[Category.LABOUR] = 0.4;
+            priorProbs[Category.COALITION] = 0.2;
+            foreach(Category c in Enum.GetValues(typeof(Category)))
+            {
+                foreach(var word in unknownDoc.Words)
+                {
+                    if(partyWords[c].Where(w => w.Key == word).DefaultIfEmpty().First() != null)
+                    {
+                        if(overallProbs.TryGetValue(c, out double value))
+                        {
+                            overallProbs[c] = value * (double)partyWords[c].Where(w => w.Key == word).FirstOrDefault().ConditionalProbability;
+                        }
+                        else
+                        {
+                            overallProbs[c] = (double)partyWords[c].Where(w => w.Key == word).FirstOrDefault().ConditionalProbability;
+                        }
+                        Console.WriteLine(overallProbs[c]);
+                    }
+                    
+                }
+                overallProbs[c] = ((double)overallProbs[c] * (double)priorProbs[c]);
+            }
+            var party = overallProbs.OrderBy(p => p.Value).First().Key;
+            Console.WriteLine(party);
             Console.ReadKey();
         }
     }
